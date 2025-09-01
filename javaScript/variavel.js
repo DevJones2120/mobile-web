9f6cb68ff21bdb9a84cbd606349b0208d6bcab30
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-01 15:53:12 -0300

    Aprendendo variável

---
+++ javaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "Olá, mundo";
+
+const nomeCompleto = "Davi Jones";
+
+let idade = 25;
+
+// Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log(" Meu nome é: " + nomeCompleto + " e minha idade é: " + idade + " Anos");
