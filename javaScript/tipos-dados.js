7b93b066bb860a0b2f0e1232533d00c264b12551
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-01 16:05:20 -0300

    Aprendendo mais variáveis

---
+++ javaScript/tipos-dados.js
@@ -0,0 +1,20 @@
+// String
+let nome = "Davi Jones"
+
+// Inteiros
+let idade = 25 
+
+// Booleno
+let casado = false;
+
+// Resultados
+let valorProduto = 5.35
+
+//Resultados
+console.log("Meu nome é " + nome) 
+console.log("Minha idade é", idade)
+console.log("Casado ?", casado)
+console.log("R$", valorProduto)
+console.log(typeof idade)
+console.log(typeof nome)
+console.log(typeof valorProduto)
