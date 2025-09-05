d0030d34fd94ab5a68b689553ff569db6bfd9b3d
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-05 14:51:52 -0300

    Aprendendo Estruturas de Decisão

---
+++ javaScript/js-exemplos/idade.js
@@ -0,0 +1,7 @@
+let idade = 18
+
+if (idade >= 18) {
+    console.log("Você pode ir para a festa !")
+} else {
+    console.log("Você não pode ir para festa")
+}
---
+++ javaScript/js-exemplos/nota.js
@@ -0,0 +1,10 @@
+// Se a nota do aluno for >= 6, o aluno está aprovado
+// Senão ele está reprovado
+
+let nota = 6;
+
+if (nota >= 6) {
+    console.log("Você foi aprovado !")
+} else {
+    console.log("Você foi reprovado")
+}
