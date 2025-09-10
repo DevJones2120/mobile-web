6a2775ffc43a3de9c6bc4e50a3f105f9610dfeec
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-10 16:08:17 -0300

    Adicionando condição if else

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -4,7 +4,23 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
 //  Função
-function resultadoNotaClick() {
-    alert("Resultado");
+function resultadoNotaClick(event) {
+    event.preventDefault()
+    // console.log(inputNota.value)
+
+    // Lógica-condição
+    if (inputNota.value >= 6) {
+        // alert("Aprovado")
+        resultado.innerHTML = "Aprovado"
+    } else {
+        // alert("Reprovado")
+        resultado.innerHTML = "Reprovado"
+    }
+
+    inputNota.value = ""
+
 }
+
+
+
 
