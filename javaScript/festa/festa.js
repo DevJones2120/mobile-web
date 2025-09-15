fbc8ea96064a316231eebd696f279eb049120f35
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-14 23:40:10 -0300

    adicionando imagens e hover

--- javaScript/festa/festa.js
+++ javaScript/festa/festa.js
@@ -3,16 +3,19 @@
 const inputIdade = document.getElementById("idade")
 const resultado = document.getElementById("result")
 const mensagem = document.getElementById("mensagem")
+const imagem = document.getElementById("projeto")
 
 //  Função
 function resultadoIdadeClick(event) {
      event.preventDefault()
 
     if (inputIdade.value >= 18) {
-        resultado.innerHTML = "Aguardo sua presença !"
-        
+        resultado.innerHTML = "Aguardo sua presença 🍻!"
+        imagem.style.display = "block";
+
     } else {
-        resultado.innerHTML = "Você não pode ir !"
+        resultado.innerHTML = "Você não pode ir ❌👮!"
+        imagem.style.display = "none";
     }
 
     // Validação de formulário
