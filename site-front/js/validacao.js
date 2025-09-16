57561f723767e106c1958d962cc5c5c920633711
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-16 16:15:56 -0300

    Adicionando requerimentos para input email

--- site-front/js/validacao.js
+++ site-front/js/validacao.js
@@ -5,13 +5,29 @@ form.addEventListener("submit", function(event) {
     
     const email = document.querySelector("#email").value.trim();
 
-
+console.log(email)
     
     // Condição de validação
     if (email === "") {
         alert("imail inválido");
         return false;
+        
     } 
+
+    // Validação simples: tem @ e .
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Email inválido, tente novamemte !");
+        return;
+
+    }
+
+    if (email.value) {
+        innerHTML = "Email cadastrado com sucesso! ✅"
+        email.style.display = "block";
+    }
+   
+    email.style.display = "none";
+
 
 
     // Limpa o campo
