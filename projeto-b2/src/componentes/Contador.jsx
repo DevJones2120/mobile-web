a04ac2840c68831010a52d41c50483edd94734d9
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-19 16:21:24 -0300

    Estilizando nosso Contador

--- projeto-b2/src/componentes/Contador.jsx
+++ projeto-b2/src/componentes/Contador.jsx
@@ -1,25 +1,29 @@
-import { Fragment } from "react"
+import "./Contador.css";
+
+import { useState } from "react";
 
 const Contador = () => {
+    // Inicializar o estado com valor 0
+    const [count, setCount] = useState(0);
 
     const aumentar = () =>  {
-        alert("Olá Rodriguinho 1");
+        setCount(count + 1);
     }
 
     const diminuir = () => {
-        alert("Olá Rodriguinho 2");
+       setCount(count - 1);
     }
 
     return (
-        <Fragment>
-            <h1>Contador</h1>
+        <main className="container">
+            <h1>{count}</h1>
 
             <section>
                 <h2>Resultado</h2>
-                <button onClick={aumentar} style={{ marginRight: "10px" }}>Aumentar</button>
-                <button onClick={diminuir}>Diminuir</button>
+                <button className="aumentar"onClick={aumentar} style={{ marginRight: "10px" }}>Aumentar</button>
+                <button className="diminuir"onClick={diminuir}>Diminuir</button>
             </section>
-        </Fragment>
+        </main>
     )
 }
 
