def83c2ab86938fc3fddb2c886e074cefe89dcbf
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-19 15:03:36 -0300

    Criando funções

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -1,20 +1,11 @@
+import Contador from "./componentes/contador";
 
 function App() {
 
   return (
     <>
-      <h1>Olá Rodriguinho 🎂</h1>
-      <hr />
-      <h2>Aniversário</h2>
-      <h3>SENAI</h3>
-      <h4>Cotia</h4>
-      <h5>Web</h5>
-      <h6>Mobile</h6>
-      <strong>Aprendendo React</strong>
-      <p>Testando <i>tags</i> html</p>
-      <div>
-        <p>TESTE <br />REACT</p>
-      </div>
+      <Contador />
+      
     </>
   )
 }
