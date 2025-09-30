d2db52a8a092784452037347c274cb64b3c74a89
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-30 15:05:42 -0300

    Criando meu app e estilizando

---
+++ meuapp/App.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+    <View style={estilos.topo}>
+      <Text style={estilos.tituloHeader}>Rodrigo Popeto</Text>
+    </View>
+
+    <View>
+      <Text>Popeto</Text>
+    </View>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: 150,
+    backgroundColor: "rgb(99, 200, 255)",
+  },
+  tituloHeader: {
+    whidth: "100%",
+    color: "white",
+    textAlign: "center",
+    paddingTop: 60,
+    fontSize: 20,
+
+  }
+});
