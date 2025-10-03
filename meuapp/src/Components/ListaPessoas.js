f82773114c4f52fc3a11c420ae81daa021a9a770
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-10-03 16:16:08 -0300

    Estilizando novo componente

--- meuapp/src/Components/ListaPessoas.js
+++ meuapp/src/Components/ListaPessoas.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function ListaPessoas() {
     const pessoas = [
@@ -26,9 +26,42 @@ export default function ListaPessoas() {
 
     ];
 
+    
   return (
-    <View>
-        <Text>Teste</Text>
+    <View style={estilos.container}>
+        <Text style={estilos.titulo}>📝Dados</Text>
+
+        {pessoas.map((pessoa) => (
+            <View style={estilos.card}>
+                <Text>Nome: {pessoa.nome}  •  </Text>
+                <Text>Idade: {pessoa.idade} anos </Text>
+            </View>
+        ))}
+
     </View>
   )
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        padding: 20,
+        marginTop: 40,
+    },
+    titulo: {
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 15,
+        textAlign: "center",
+    },
+    card: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 8,
+        padding: 12,
+        backgroundColor: "rgba(252, 252, 252, 1)",
+        borderWidth: 2,
+        borderColor:  "rgb(220, 20, 60)",
+        borderRadius: 10,
+        elevation: 8,
+    }
+})
