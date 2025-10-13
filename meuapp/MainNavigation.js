6eb96d892b82619393b1d43dd4d22668d0ec6635
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-10-13 15:01:35 -0300

    Criando página Faq e navegação

--- meuapp/MainNavigation.js
+++ meuapp/MainNavigation.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./src/Telas/Home";
 import Sobre from "./src/Telas/Sobre";
+import Faq from "./src/Telas/Faq";
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,8 @@ export default function MainNavigation() {
         <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
             <Stack.Screen name="Sobre" component={Sobre} />
+            <Stack.Screen name="Faq" component={Faq} />
+
         </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ meuapp/src/Telas/Faq.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default function Faq() {
+  return (
+    <View>
+        <Text style = {estilos.texto}>Faq</Text>
+
+    </View>
+  )
+}
+
+const estilos = StyleSheet ({
+    texto: {
+        
+    }
+})
