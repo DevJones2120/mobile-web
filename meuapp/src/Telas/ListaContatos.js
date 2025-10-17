f440c2fc0512ddb3fc5dff4c422fb743fc2dd513
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-10-17 16:16:16 -0300

    Puxando API e estilizando

--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import axios from "axios";
 
 export default function ListaContatos() {
@@ -23,8 +23,47 @@ export default function ListaContatos() {
     }, [])
 
   return (
-    <View>
-        <Text>Lista de Contatos</Text>
+    <View style={estilos.container}>
+        <Text style={estilos.titulo}>Lista de Contatos</Text>
+        {contatos.length > 0 ? (
+            contatos.map((contato, index) => (
+                <View key={index}>
+                    <Text style={estilos.nome}>👤 Nome: {contato.nome}</Text>
+                    <Text style={estilos.telefone}>📞 Telefone: {contato.telefone}</Text>
+                </View>
+            ))
+        ) : (
+            <Text >Nenhum contato disponível</Text>
+        )}
     </View>
   )
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        marginLeft: 50,
+        marginRight: 50,
+        borderWidth: 2,
+        borderColor: "rgb(254, 56, 92)",
+    },
+
+    titulo: {
+        fontSize: 20,
+        paddingTop: 25,
+        paddingLeft: 80,
+        paddingBottom: 25,
+        fontWeight: "bold",
+    },
+    nome: {
+        fontSize: 16,
+        padding: 10,
+        
+
+    },
+    telefone: {
+        fontSize: 16,
+        padding: 10,
+        
+
+    },
+})
