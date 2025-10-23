cd538dba4b3a20b2b75695500bcdb4dbd5f2987e
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-10-23 16:19:37 -0300

    Estilizando páginas Lista contatos e Cadastro

--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Text, View, StyleSheet, Image, ScrollView, Button, Alert, TouchableOpacity } from 'react-native';
 import axios from "axios";
 import imagem2 from "../../assets/imgPng-2.png"
+import imagem6 from "../../assets/imgPng-6.png"
 
 export default function ListaContatos() {
     const [contatos, setContatos] = useState ([]);
@@ -45,12 +46,11 @@ export default function ListaContatos() {
         {contatos.length > 0 ? (
             contatos.map((contato, index) => (
                 <View style={estilos.dados} key={index}>
+                    <Image style={estilos.imgUser} source={imagem6}/>
                     <Text style={estilos.nome}>👤 Nome: {contato.nome}</Text>
                     <Text style={estilos.telefone}>📞 Telefone: {contato.telefone}</Text>
-                    <TouchableOpacity>
-                        color="rgb(252, 46, 85)"
-                        title="Excluir"
-                        onPress={() => deleteContato(contato.id)}
+                    <TouchableOpacity style={estilos.button} onPress={() => deleteContato(contato.id)}>
+                        <Text style={estilos.titleButton}>Excluir</Text>
                     </TouchableOpacity>
                 </View>
             ))
@@ -67,11 +67,26 @@ const estilos = StyleSheet.create({
         marginRight: 20,
     },
     dados: {
-        borderRadius: 20,
+        margin: 15,
+        marginVertical: 1,
+    },
+    button: {
+        borderRadius: 10,
         borderWidth: 2,
         borderColor: "rgba(223, 38, 72, 1)",
-        padding: 8,
-        margin: 20,
+        backgroundColor: "rgb(234, 34, 100)",
+        alignItems: "center",
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginLeft: 50,
+        marginRight: 50,
+        margin: 10,
+    },
+    titleButton: {
+        borderColor: "#000000ff",
+        color: "#FFF",
+        fontWeight: "bold",
+        fontSize: 20,
     },
     titulo: {
         fontSize: 25,
@@ -83,19 +98,21 @@ const estilos = StyleSheet.create({
     img: {
         width: 250,
         height: 250,
-        marginLeft: 50 ,
-        shadowColor: "rgb(234, 34, 100)",
-        shadowOpacity: 0.9,
-        elevation: 20,
+        marginLeft: 50
+    },
+    imgUser: {
+        width: 150,
+        height: 150,
+        marginLeft: 90
     },
     nome: {
         fontSize: 16,
+        paddingLeft: 60,
         padding: 10,
     },
     telefone: {
         fontSize: 16,
+        paddingLeft: 60,
         padding: 10,
-        
-
-    },
+    }
 })
